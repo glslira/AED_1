7f3a01360b7647704243fdca84125ee45260ba55
glslira <gabriel.glira03@gmail.com>
2025-02-12 19:33:30 -0300

    Add files via upload

---
+++ 1036.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    double a, b, c, delta, R1, R2;
+
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    delta = b * b - 4 * a * c;
+
+    if (a != 0 && delta >= 0) {
+        R1 = (-b + sqrt(delta)) / (2 * a);
+        R2 = (-b - sqrt(delta)) / (2 * a);
+
+        printf("R1 = %.5lf\nR2 = %.5lf\n", R1, R2);
+    } else {
+        printf("Impossivel calcular\n");
+    } //else
+
+    return 0;
+} //main
